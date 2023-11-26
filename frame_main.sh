43c5f891bf344a892e8449d79335651da86ced63
JavaRaf <138421856+JavaRaf@users.noreply.github.com>
2023-11-26 12:04:45 -0300

    EP 04

--- frame_main.sh
+++ frame_main.sh
@@ -9,7 +9,7 @@ time_started="$(TZ="${sys_timezone}" date)"
 
 # Main Loop
 for ((i=1;i<=fph;i++)); do
-    bash -x ./frameposter.sh "${1}" "${2}" || bash -x img_process.sh "failed" "${time_started}" || exit 1
+    bash ./frameposter.sh "${1}" "${2}" || bash img_process.sh "failed" "${time_started}" || exit 1
     sleep "$((mins * 60))"
 done
 
@@ -20,7 +20,7 @@ lim_frame="$((prev_frame+fph-1))"
 time_ended="$(TZ="${sys_timezone}" date)"
 if [[ "${desc_update}" == "1" ]]; then
     ovr_all="$(sed -E ':L;s=\b([0-9]+)([0-9]{3})\b=\1,\2=g;t L' counter_n.txt)"
-    abt_txt="$(eval "printf '%s' \"$(sed -E 's_\{\\n\}_\n_g;s_\{([^\x7d]*)\}_\${\1:-??}_g;s|ovr_all:-\?\?|over_all:-0|g' <<< "${abt_txt}"\")")"
+    abt_txt="$(eval "printf '%s' \"$(sed -E 's_\{\\n\}_\n_g;s_\{([^\x7d]*)\}_\${\1:-??}_g;s|ovr_all:-\?\?|ovr_all:-0|g' <<< "${abt_txt}"\")")"
     curl -sLk -X POST "https://graph.facebook.com/me/?access_token=${1}" --data-urlencode "about=${abt_txt}" -o /dev/null || true
 fi
-bash -x img_process.sh "success" "${prev_frame}" "${lim_frame}" "${time_started}" "${time_ended}"
+bash img_process.sh "success" "${prev_frame}" "${lim_frame}" "${time_started}" "${time_ended}"
